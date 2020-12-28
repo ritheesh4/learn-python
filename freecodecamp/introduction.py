fd80afb6f239e322dcc7a16ee496e04367947990
Ritheesh <ritheesh.4@gmail.com>
2020-12-28 13:59:29 +0530

    while loop for loop

---
+++ freecodecamp/introduction.py
@@ -0,0 +1,25 @@
+# Type indentification
+
+# testString = 'hello ritheesh'
+# stringToInt = int(testString)
+# type(stringToInt)
+
+
+# Defining a fucntion
+
+# def add_two_numbers(a,b):
+#     return a+b
+# print(add_two_numbers(2,3))
+
+#While loop
+# n=0
+# while True:
+#     if n==3:
+#         break
+#     print(n)
+#     n=n+1
+
+
+# for loop
+for i in [2,1,5]:
+    print(i)
